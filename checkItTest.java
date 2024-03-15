459a7f7dc4398d4891c1fefba43975e52c3a04c5
RyanGuzelian <ryanguzelian@gmail.com>
2024-03-15 12:51:59 -0400

    Added predicate coverage

---
+++ checkItTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class checkItTest {
+
+    @Test
+    public void testCheckItPredicateCoverage1() {
+        assertTrue(CheckIt.checkIt(true, false, false));
+    }
+
+    @Test
+    public void testCheckItPredicateCoverage2() {
+        assertFalse(CheckIt.checkIt(false, true, false));
+    }
+
+    
+}
